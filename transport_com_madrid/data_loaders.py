235efe15db079a28a62ef1da96612555d323977c
Manuel Castillo <manucalop@gmail.com>
2022-02-12 15:34:01 +0100

    Data loaders developed and unit tested

--- transport_com_madrid/data_loaders.py
+++ transport_com_madrid/data_loaders.py
@@ -1,59 +1,75 @@
-from abc import abstractmethod
-from locations import GPSLocation
+from abc import abstractmethod, ABC
 import pandas as pd
+# Local imports
+from .locations import ILocation
+from .utils import xy2gps
 
-class DataLoader:
+class IStationsDataLoader(ABC):
     @abstractmethod
-    def get_df(self) -> pd.DataFrame:
+    def load_data(self):
         pass
 
-class BicycleDataLoader(DataLoader):
+    @abstractmethod
+    def get_nearby(self, location : ILocation, max_distance : float) -> pd.DataFrame:
+        pass
+
+    @abstractmethod
+    def get_station(self, idx : int = -1, name : str = '') -> pd.DataFrame:
+        pass
+
+class BicycleStationsDataLoader(IStationsDataLoader):
     def __init__(self, filename : str):
+        # Load data on creation
         self.filename = filename
-    def get_df(self) -> pd.DataFrame:
-        return pd.read_excel('./../data/2018_Julio_Bases_Bicimad_EMT.xlsx')
+        self.load_data()
 
+    def load_data(self):
+        self.data = pd.read_excel(self.filename)
+        # Drop location missings
+        self.data.dropna(subset=['latitude','longitude'], inplace=True)
+        self.data.set_index('id', inplace=True, drop=False)
 
-class User(GPSLocation):
-    def __init__(self, lat : float, lon : float):
-        super().__init__(lat, lon)
-    def get_bicycle_stations(self, max_distance : float):
-        # Query data
-        df = pd.read_excel('./../data/2018_Julio_Bases_Bicimad_EMT.xlsx')
-        # Drop any location missing
-        df = df.dropna(subset=['latitude','longitude'])
-        # Get distance
-        df['distance'] = df[['latitude','longitude']].apply(self.get_distance, axis='columns')
+    def get_nearby(self, location : ILocation, max_distance : float) -> pd.DataFrame:
+        df = self.data
+        # Add distance column
+        df['distance'] = df[['latitude','longitude']].apply(location.get_distance, axis='columns')
         # Filter out those exceeding maximum distance
         df = df[df['distance'] < max_distance]
         # Return ordered list
         return df.sort_values('distance')
 
+    def get_station(self, idx : int = -1, name : str = '') -> pd.DataFrame:
+        return self.data[(self.data['id'] == idx) |
+                         (self.data['name'] == name)]
 
-class Station(GPSLocation):
-    def __init__(self, lat : float, lon : float, idx : int, name : str):
-        super().__init__(lat, lon)
-        self.idx = idx
-        self.name = name
+class BusStationsDataLoader(IStationsDataLoader):
+    def __init__(self, filename):
+        # Load data on creation
+        self.filename = filename
+        self.load_data()
 
-class Vehicle(GPSLocation):
-    def __init__(self, lat : float, lon : float):
-        super().__init__(lat, lon)
+    def load_data(self):
+        df = pd.read_csv('./data/buses_madrid.csv')
+        #Drop missing location values
+        df.dropna(subset=['X','Y'], inplace=True)
+        #Get latitude and longitude columns
+        df[['longitude','latitude']] = df[['X','Y']].apply(xy2gps, axis='columns')
+        #Clean id column
+        df['IDESTACION'] = pd.to_numeric(df['IDESTACION'].str[2:])
+        #Set it as index
+        df.set_index('IDESTACION', inplace=True, drop=False)
+        #Save it
+        self.data = df
 
-class Bicycle(Vehicle):
-    def __init__(self, lat : float, lon : float):
-        super().__init__(lat, lon)
+    def get_nearby(self, location : ILocation, max_distance : float) -> pd.DataFrame:
+        df = self.data
+        # Add distance column
+        df['distance'] = df[['latitude','longitude']].apply(location.get_distance, axis='columns')
+        # Filter out those exceeding maximum distance
+        df = df[df['distance'] < max_distance]
+        # Return ordered list
+        return df.sort_values('distance')
 
-# df = pd.read_excel('./../data/2018_Julio_Bases_Bicimad_EMT.xlsx')
-# print(df.head())
-# print(df.columns)
-user = User(lat=40.44, lon=-3.69)
-print(user.get_bicycle_stations(100000000000000000000))
-
-#func = lambda x : x['latitude'] + x['longitude']
-
-#df = pd.read_excel('./../data/2018_Julio_Bases_Bicimad_EMT.xlsx')
-##Transform data
-#df['distance'] = df[['latitude','longitude']].apply(func, axis='columns')
-#df = df.sort_values('distance')
-#print(df)
+    def get_station(self, idx : int = -1, name : str = '') -> pd.DataFrame:
+        return self.data[(self.data['IDESTACION'] == idx) |
+                         (self.data['DENOMINACION'] == name)]
